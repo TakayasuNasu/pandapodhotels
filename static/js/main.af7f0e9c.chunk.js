d1889321ee0909d43943ac3fc289237318c4190c
TakayasuNasu <taka.beckham@gmail.com>
2021-03-19 03:27:50 +0000

    Updates

--- static/js/main.af7f0e9c.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpmy-app"]=this["webpackJsonpmy-app"]||[]).push([[0],{9:function(e,t,n){"use strict";n.r(t);var c=n(1),r=n.n(c),s=n(3),a=n.n(s),p=n(0),i=function(){return Object(p.jsx)("h1",{children:"Panda"})};a.a.render(Object(p.jsx)(r.a.StrictMode,{children:Object(p.jsx)(i,{})}),document.getElementById("root"))}},[[9,1,2]]]);
-//# sourceMappingURL=main.af7f0e9c.chunk.js.map
