fb5d3faab543cc06922da39f041a7b7e1ffbb89e
takayasu.nasu <taka.beckham@gmail.com>
2021-03-18 20:33:39 -0700

    Modified image path.

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -23,25 +23,25 @@ const Index = () => {
   return (
     <Wrapper>
       <ListStyle>
-        <li><img src="./pandapodhotels/img/1.jpg" alt=""/></li>
-        <li><img src="./pandapodhotels/img/20190430-_MG_0019.jpg" alt=""/></li>
-        <li><img src="./pandapodhotels/img/20190430-_MG_0060.jpg" alt=""/></li>
-        <li><img src="./pandapodhotels/img/20190430-_MG_0061.jpg" alt=""/></li>
-        <li><img src="./pandapodhotels/img/20190513-_MG_0365.jpg" alt=""/></li>
-        <li><img src="./pandapodhotels/img/20191023-_MG_4385.jpg" alt=""/></li>
-        <li><img src="./pandapodhotels/img/20191023-_MG_4390.jpg" alt=""/></li>
-        <li><img src="./pandapodhotels/img/20191023-_MG_4449.jpg" alt=""/></li>
-        <li><img src="./pandapodhotels/img/20191023-_MG_4474.jpg" alt=""/></li>
-        <li><img src="./pandapodhotels/img/20191023-_MG_4483.jpg" alt=""/></li>
-        <li><img src="./pandapodhotels/img/Copy of 20190430-_MG_0060.jpg" alt=""/></li>
-        <li><img src="./pandapodhotels/img/Copy of 20190430_163959.jpg" alt=""/></li>
-        <li><img src="./pandapodhotels/img/Copy of 20191023-_MG_4441.jpg" alt=""/></li>
-        <li><img src="./pandapodhotels/img/Copy of IMG_20190531_183552.jpg" alt=""/></li>
-        <li><img src="./pandapodhotels/img/LOGO2 (1).jpg" alt=""/></li>
-        <li><img src="./pandapodhotels/img/Logo.PNG" alt=""/></li>
-        <li><img src="./pandapodhotels/img/Logo.jpg" alt=""/></li>
-        <li><img src="./pandapodhotels/img/PandaPod_IG_12.jpg" alt=""/></li>
-        <li><img src="./pandapodhotels/img/PandaPod_IG_17.jpg" alt=""/></li>
+        <li><img src="./img/1.jpg" alt=""/></li>
+        <li><img src="./img/20190430-_MG_0019.jpg" alt=""/></li>
+        <li><img src="./img/20190430-_MG_0060.jpg" alt=""/></li>
+        <li><img src="./img/20190430-_MG_0061.jpg" alt=""/></li>
+        <li><img src="./img/20190513-_MG_0365.jpg" alt=""/></li>
+        <li><img src="./img/20191023-_MG_4385.jpg" alt=""/></li>
+        <li><img src="./img/20191023-_MG_4390.jpg" alt=""/></li>
+        <li><img src="./img/20191023-_MG_4449.jpg" alt=""/></li>
+        <li><img src="./img/20191023-_MG_4474.jpg" alt=""/></li>
+        <li><img src="./img/20191023-_MG_4483.jpg" alt=""/></li>
+        <li><img src="./img/Copy of 20190430-_MG_0060.jpg" alt=""/></li>
+        <li><img src="./img/Copy of 20190430_163959.jpg" alt=""/></li>
+        <li><img src="./img/Copy of 20191023-_MG_4441.jpg" alt=""/></li>
+        <li><img src="./img/Copy of IMG_20190531_183552.jpg" alt=""/></li>
+        <li><img src="./img/LOGO2 (1).jpg" alt=""/></li>
+        <li><img src="./img/Logo.PNG" alt=""/></li>
+        <li><img src="./img/Logo.jpg" alt=""/></li>
+        <li><img src="./img/PandaPod_IG_12.jpg" alt=""/></li>
+        <li><img src="./img/PandaPod_IG_17.jpg" alt=""/></li>
       </ListStyle>
     </Wrapper>
   )
